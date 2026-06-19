c28a4feeaf2d3227d097442e5380773f025ce249
Seyedalimoosavi369 <151033650+Seyedalimoosavi369@users.noreply.github.com>
2026-06-20 00:08:04 +0330

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,28 +1,23 @@
 function renderShop() {
     const shopContainer = document.getElementById('shop-items');
-    shopContainer.innerHTML = '';
-
+    shopContainer.innerHTML = '<h3>آیتم‌های پایه (TRX)</h3>';
+    
     items.forEach(item => {
         shopContainer.innerHTML += `
             <div class="card">
                 <div><strong>Item ${item.id}</strong><br>Level: ${item.level}</div>
-                <button onclick="buyItem(${item.id})">Buy</button>
+                <button onclick="buyItem(${item.id})">Buy (${item.baseCost * Math.pow(2, item.level - 1)} TRX)</button>
             </div>`;
     });
 
+    shopContainer.innerHTML += '<h3>آیتم‌های خاص (TON)</h3>';
     Object.keys(specialItems).forEach(key => {
         if (key !== 'zeus') {
             shopContainer.innerHTML += `
                 <div class="card" style="border: 2px solid gold;">
                     <div><strong>${specialItems[key].name}</strong><br>Level: ${specialItems[key].level}</div>
-                    <button onclick="upgradeSpecial('${key}')">Upgrade TON</button>
+                    <button onclick="upgradeSpecial('${key}')">Buy (${specialItems[key].cost} TON)</button>
                 </div>`;
         }
     });
-
-    shopContainer.innerHTML += `
-        <div class="card" style="border: 2px solid cyan;">
-            <div><strong>Zeus Count: ${specialItems.zeus}</strong></div>
-            <button onclick="craftZeus()">Craft</button>
-        </div>`;
 }
