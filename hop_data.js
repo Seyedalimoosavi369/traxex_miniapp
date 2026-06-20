5308029f62e780920d691d1946a8f482c3b2d1d1
Seyedalimoosavi369 <151033650+Seyedalimoosavi369@users.noreply.github.com>
2026-06-20 13:51:26 +0330

    Create hop_data.js

---
+++ hop_data.js
@@ -0,0 +1,21 @@
+const shopConfig = {
+    items: [
+        { id: 1, name: "Iron Pickaxe", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 2, name: "Bronze Drill", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 3, name: "Silver Chisel", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 4, name: "Gold Hammer", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 5, name: "Platinum Blade", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 6, name: "Diamond Cutter", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 7, name: "Titanium Axe", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 8, name: "Ruby Saw", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 9, name: "Emerald Shovel", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 10, name: "Sapphire Pick", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 11, name: "Obsidian Maul", baseTRX: 1000, baseTON: 1, level: 0 },
+        { id: 12, name: "Void Breaker", baseTRX: 1000, baseTON: 1, level: 0 }
+    ],
+    specials: {
+        rexar: { level: 0, priceTON: 10, unlocked: false },
+        soulKeeper: { level: 0, priceTON: 10, unlocked: false },
+        zeus: { level: 0, priceTON: 100, unlocked: false }
+    }
+};
