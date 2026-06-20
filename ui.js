c6078b0b78091cc02ffbeb7a0eaa7973be514293
Seyedalimoosavi369 <151033650+Seyedalimoosavi369@users.noreply.github.com>
2026-06-20 09:04:36 +0330

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,7 +1,6 @@
 function renderShop() {
     const shopContainer = document.getElementById('shop-items');
     shopContainer.innerHTML = '<h3>آیتم‌های پایه (TRX)</h3>';
-    
     items.forEach(item => {
         shopContainer.innerHTML += `
             <div class="card">
@@ -10,12 +9,11 @@ function renderShop() {
                 <button onclick="buyItem(${item.id})">Buy (${item.baseCost * Math.pow(2, item.level - 1)})</button>
             </div>`;
     });
-
     shopContainer.innerHTML += '<h3>آیتم‌های خاص (TON)</h3>';
     Object.keys(specialItems).forEach(key => {
         if (key !== 'zeus') {
             shopContainer.innerHTML += `
-                <div class="card" style="border: 2px solid gold;">
+                <div class="card">
                     <img src="${key}.png" onerror="this.style.display='none'">
                     <div><strong>${specialItems[key].name}</strong><br>Level: ${specialItems[key].level}</div>
                     <button onclick="upgradeSpecial('${key}')">Buy (${specialItems[key].cost})</button>
